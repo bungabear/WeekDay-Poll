fa4e0acc1da31e25eca92be77b2f41ef79ab8ba1
Minjae Son <bungabear6422@gmail.com>
2018-06-08 03:22:40 +0900

    테이블 저장형태 변경, 사용자 추가, 선택 기능 구현
    자신과 다른 사용자를 구분하지 않고 3차원 배열로 각 셀에
    사용자 목록을 넣도록 변경.
    Bottom Sheel Modal로 사용자를 추가하거나 고르도록 적용.
    Preload외에 로딩을 사용할 수 있도록 Opacity가 조절 가능한
    showLoading(opacity), hideLoading() 작성.

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,19 @@
-function showLoader(){
+document.addEventListener("DOMContentLoaded", ()=>{
     $('.preloader-background').delay(500).fadeOut('slow');
-	
-	$('.preloader-wrapper')
-		.delay(500)
-		.fadeOut();
-}
+    $('.preloader-wrapper')
+        .delay(500)
+        .fadeOut();
+});
 
-document.addEventListener("DOMContentLoaded", showLoader);
+function hideLoading(){
+    $('.preloader-background').fadeOut('slow');
+    $('.preloader-wrapper')
+        .fadeOut();
+    
+}
+// TODO show str
+function showLoading(str = "", opacity = 0.5){
+    $('.preloader-background').css("opacity", opacity);
+    $('.preloader-background').fadeIn();
+    $('.preloader-wrapper').fadeIn();
+}
